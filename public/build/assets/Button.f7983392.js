5dc98a90085ac42976b833cfc43c70b03aed4886
Edo Pranata <edo.pranata@gmail.com>
2022-12-11 11:31:29 +0700

    Publish Beta

---
+++ public/build/assets/Button.f7983392.js
@@ -0,0 +1,1 @@
+import{o,c as n,g as s}from"./app.2f6d9169.js";const r=["type"],p={__name:"Button",props:{type:{type:String,default:"button"}},setup(t){return(e,a)=>(o(),n("button",{type:t.type,class:"btn tracking-widest"},[s(e.$slots,"default")],8,r))}};export{p as _};
